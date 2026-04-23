73ecc8fd2d0e19cb470dcda325991a697c917595
Sudhikshan7 <sudhikshanchithramani@gmail.com>
2026-04-23 18:16:29 +1000

    Starting names are given to each pages

---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,5 @@
+// app/(dashboard)/jobs/page.tsx
+
+export default function JobsPage() {
+  return <h1 className="text-2xl font-semibold">Jobs</h1>;
+}
---
+++ app/(dashboard)/leagues/page.tsx
@@ -0,0 +1,5 @@
+// app/(dashboard)/reports/page.tsx
+
+export default function LeaguesPage() {
+  return <h1 className="text-2xl font-semibold">Leagues</h1>;
+}
